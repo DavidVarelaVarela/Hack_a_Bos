6e29598fabda6fed92d9d230050766b137095625
DavidVarelaVarela <davidvarela87@gmail.com>
2019-09-05 21:30:49 +0200

    notas

--- JS/test.js
+++ JS/test.js
@@ -1,6 +1,8 @@
-'use strict'
+let 1 = dormir;
+let 2 = `los domingo por la tarde me gusta ${1}`;
 
-
+// estudiar el tema de las comillas y como se interpolan los valores
+// estudiar parseint
 
 
 
