9d0e199e6152234475deeeb21b40aeb7b7002a0b
DavidVarelaVarela <davidvarela87@gmail.com>
2019-09-09 19:41:57 +0200

    modificaciones

---
+++ Módulo_2_JS/Clase _9-9-19/mod2-js1-master/Dia-3.md
@@ -0,0 +1,16 @@
+# Día 3
+
+## Funciones
+
+[Funciones en MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Funciones)
+
+
+## Bucles
+
+[Bucles e iteración en MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Bucles_e_iteraci%C3%B3n)
+
+[While en MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Sentencias/while)
+
+[do...while en MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Sentencias/do...while)
+
+[For en MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Sentencias/for)
---
+++ Módulo_2_JS/Clase _9-9-19/mod2-js1-master/starter-project/www/js/src.js
@@ -0,0 +1,60 @@
+'use strict';
+
+//Declaración de funciones
+
+function estaEsSuperFacil() {
+    console.log("FACILISIMO")
+    
+};
+//Expresión de función
+let estaEsOtraManera = function name(params) {
+    console.console.log("FACILISIMO TAMBIEN");
+    
+};
+
+estaEsOtraManera ();
+
+
+let estaEsOtraManera_2 = () => {
+console.console.log(FACILISIMO);
+
+}
+estaEsOtraManera_2 ();
+
+IIFE // Autollamado de función
+
+
+(function(){
+console console.log(va sola);
+()
+
+}
+)
+
+
+function eco(queDigo) {
+    console.log(queDigo)
+}
+let leDigoEsto = "esto";
+eco(leDigoEsto);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
