6301f3777a167f2c24b7a1ef06931dd935640a7f
DavidVarelaVarela <davidvarela87@gmail.com>
2019-09-09 19:50:07 +0200

    mod

--- Módulo_2_JS/Clase _9-9-19/mod2-js1-master/starter-project/www/js/src.js
+++ Módulo_2_JS/Clase _9-9-19/mod2-js1-master/starter-project/www/js/src.js
@@ -1,80 +1,55 @@
-'use strict';
+"use strict";
 
 //Declaración de funciones
 
 function estaEsSuperFacil() {
-    console.log("FACILISIMO")
-    
-};
+  console.log("FACILISIMO");
+}
 //Expresión de función
 let estaEsOtraManera = function name(params) {
-    console.console.log("FACILISIMO TAMBIEN");
-    
+  console.console.log("FACILISIMO TAMBIEN");
 };
 
-estaEsOtraManera ();
-
+estaEsOtraManera();
 
 let estaEsOtraManera_2 = () => {
-console.console.log(FACILISIMO);
+  console.console.log(FACILISIMO);
+};
+estaEsOtraManera_2();
 
-}
-estaEsOtraManera_2 ();
+// IIFE  Autollamado de función
 
-IIFE // Autollamado de función
-
-
-(function(){
-console.log(va_sola);
-()
-
-}
-)
-
+(function() {
+  console.log(va_sola);
+});
 
 function eco(queDigo) {
-    console.log(queDigo)
+  console.log(queDigo);
 }
 let leDigoEsto = "esto";
 eco(leDigoEsto);
 
-
-
-
-
-
 //TODO: ESTUDIAR LAS FUNCIONES
 
 function patasAnimal(animal) {
-    let patas;
-    switch (animal) {
-        case 'perro':
-            patas = 4;
-        break;
-        case 'araña':
-            patas = 8;
-        break;
-        case 'cienpies':
-            patas = 100;
-        break;
-        case 'canguro':
-            patas = 2;
-        break;
-        default: "NO TIENE PATAS"
-        break;
-    }
-    
+  let patas;
+  switch (animal) {
+    case "perro":
+      patas = 4;
+      break;
+    case "araña":
+      patas = 8;
+      break;
+    case "cienpies":
+      patas = 100;
+      break;
+    case "canguro":
+      patas = 2;
+      break;
+    default:
+      "NO TIENE PATAS";
+      break;
+  }
 }
 
-console.log(patasAnimal('perro'))
-
-
-
-
-
-
-
-
-
-
-
+console.log(patasAnimal("perro"));
