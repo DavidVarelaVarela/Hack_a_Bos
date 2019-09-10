8fa0aae48e079c3d00c26e19377ae44ed8da7d2e
DavidVarelaVarela <davidvarela87@gmail.com>
2019-09-10 19:00:53 +0200

    a

--- Módulo_2_JS/Clase_10-9-19/www/js/test.js
+++ Módulo_2_JS/Clase_10-9-19/www/js/test.js
@@ -2,15 +2,14 @@
 let unaVariable = prompt();
 let numeroQueDesactiva = Math.round(Math.round()*10)
 for (let index = 0; index < array.length; index++) {
-    const  = array[1,2,3,4,5];
-    if (array === unaVariable) {
+    const  "una" = array[1,2,3,4,5];
+    if (una=== unaVariable) {
         break;
         else{
-           console.log = "BOOM!"
         }
     }
     
-}
+} 
 
 console.log= numeroQueDesactiva()
 
