f7d94206d4e44ee7631a64b0d64fd6dfff7c3060
DavidVarelaVarela <davidvarela87@gmail.com>
2019-09-10 18:45:50 +0200

    oo

---
+++ Módulo_2_JS/Clase_10-9-19/www/js/test.js
@@ -0,0 +1,18 @@
+
+let unaVariable = prompt();
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[1,2,3,4,5];
+    if (array === unaVariable) {
+        break;
+        else{
+           console.log = "BOOM!"
+        }
+    }
+    
+}
+
+
+
+
+
