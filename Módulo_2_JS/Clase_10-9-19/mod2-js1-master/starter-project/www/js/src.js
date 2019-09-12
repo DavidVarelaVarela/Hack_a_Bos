ffbb88e1ae0cd7616035f2b97ff81d5c4dee01b0
DavidVarelaVarela <davidvarela87@gmail.com>
2019-09-12 21:00:14 +0200

    cambios

--- Módulo_2_JS/Clase_10-9-19/mod2-js1-master/starter-project/www/js/src.js
+++ Módulo_2_JS/Clase_10-9-19/mod2-js1-master/starter-project/www/js/src.js
@@ -1,25 +1,37 @@
-'use strict';
+`use strict`
 
+/*const elDatoOriginal = [1,2,3,4,5,6,7,8];
 
-function desactiveLaBomba() {
-    let codigo = Math.round(Math.random()*10);
-    console.log(codigo);
-    for (let i = 0; i < 5; i++) {
-        let intento = parseInt(prompt());
-        if (codigo === intento) {
-            return true;
-        }
-    }
-    return false
-}                                          
+function datoVuelta(elDato) {
+  const alReves =1;
+  for (let i = elDato.length -1; i >=0; i--) {
+    alReves.push(elDato(i))
+  ];
+  return alReves
+  }
+ 
 
-let acerte = desactiveLaBomba()
+console.log= (alReves)
 
-if (acerte) {
-    console.log('desactivado')
-} else {
-    console.log('BOOM');
+*/
+
+let enfermos =[true,false,true,false,false,false,true,false];
+let sanos = [...enfermos];
+for (let i = 0 ;i < enfermos.length; i++) {
+  if (enfermos[i]===true && i !=0 && i !=enfermos.length ){
+    sanos[ i +1] = true;
+    sanos [i -1] = true;
+   
+  }
+if (enfermos[i]=== true) {
+  enfermos [i] = false;
 }
+  
+}
+console.log(sanos);
+
+
+
 
 
 
