91af72ab241ac0bb7a00cf52a9e391b102d213bf
DavidVarelaVarela <davidvarela87@gmail.com>
2019-09-13 18:46:27 +0200

    a

--- Módulo_2_JS/Clase_13-9-19/mod2-js1-master/starter-project/www/js/src.js
+++ Módulo_2_JS/Clase_13-9-19/mod2-js1-master/starter-project/www/js/src.js
@@ -1,1 +1,20 @@
 'use strict';
+
+
+function generarPiramide(base){
+    for (i=1; i<=base; i++){
+        for (j=0; j<i; j++){
+              document.write("*");
+        }
+        document.write("<br />");
+    }
+}
+
+
+
+
+
+
+
+
+
