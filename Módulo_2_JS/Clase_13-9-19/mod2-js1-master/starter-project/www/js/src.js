c82a43797206c416a919d06fe8d4526c14220ef5
DavidVarelaVarela <davidvarela87@gmail.com>
2019-09-13 18:48:51 +0200

    b

--- Módulo_2_JS/Clase_13-9-19/mod2-js1-master/starter-project/www/js/src.js
+++ Módulo_2_JS/Clase_13-9-19/mod2-js1-master/starter-project/www/js/src.js
@@ -1,14 +1,27 @@
 'use strict';
 
 
-function generarPiramide(base){
-    for (i=1; i<=base; i++){
-        for (j=0; j<i; j++){
-              document.write("*");
-        }
-        document.write("<br />");
+function espacios(x){
+    for( i=0;i<x;i++)
+     document.write("  ");//espacio en blanco
+   }
+    
+   function asteriscos( x){
+    for( i=0;i<x;i++)
+      document.write("*");
+   }
+    
+   function piramide(){
+    
+    var a=5;
+    
+    for(i=a;i>=0;i--){
+       espacios(i+1);
+       asteriscos(a-i);
+       document.write("<br />");
     }
-}
+    
+   }
 
 
 
