e1ad968273f8ae49b85c3470b69cdd1c2bc35ecb
DavidVarelaVarela <davidvarela87@gmail.com>
2019-09-13 20:31:26 +0200

    a

--- Módulo_2_JS/Clase_13-9-19/mod2-js1-master/starter-project/www/js/src.js
+++ Módulo_2_JS/Clase_13-9-19/mod2-js1-master/starter-project/www/js/src.js
@@ -25,15 +25,53 @@ function espacios(x){
 
 */
 
-function crearPiramide(base) {
-    for (let i = 0; i < base; i++) {
-        if (base <= 10) {
-            console.log(base);
+function crearPiramide(numeroPisos) {
+    for (let i = 0; i < numeroPisos; i++) {
+    let piso = ''
+        for (let k = 0; k <= numeroPisos - i; k++) {
+            piso = piso + ' ';        
         }
-       
+        for (let j = 0; j <= i; j++) {
+            piso = piso + '*';        
+        }
+    console.log(piso)   
         
     }    
 }
+
+crearPiramide(5);
+
+var cadena = "* ";
+var piramide="";
+
+//con for
+for(var i =0; i<5; i++){
+    piramide +=cadena;
+    document.write(piramide+ "<br>");
+
+
+}
+
+//con while
+var contador=0;
+piramide="";
+while(contador <5){
+    piramide +=cadena;
+    document.write(piramide+ "<br>");
+    contador++;
+
+}
+
+//con do while
+contador=0;
+piramide="";
+
+do{
+ piramide+=cadena;
+ document.write(piramide + "<br>");
+ contador++;
+
+}while(contador<5)
 
 
 
