34fe7ec1b406ee9bb34032f10bbfbeedb6e42bdc
DavidVarelaVarela <davidvarela87@gmail.com>
2019-09-14 13:59:51 +0200

    a

--- Módulo_2_JS/Clase_14-9-19/mod2-js1-master/starter-project/www/js/src.js
+++ Módulo_2_JS/Clase_14-9-19/mod2-js1-master/starter-project/www/js/src.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import { callbackify } from "util";
 
 //Ordenar numero de menos a mayor
 /*
@@ -10,32 +11,49 @@ console.log(desordenado.sort(function (a, b){
 }))
 console.log(desordenado.sort((a, b) => a - b ))
 */
-const desordenado = [7,5,10,3,4]
+/*
+const desordenado = [7, 5, 10, 3, 4];
 
 function ordenaElArray(elDato) {
-    const datoOrdenado = [...elDato];
-     
-    for (let i = 0; i< datoOrdenado.length; i++) {
-        for (let j= 0; j < datoOrdenado.length; j++) {
-            if (datoOrdenado[i] > datoOrdenado[j]){} 
-            const temp = datoOrdenado[j];
-            datoOrdenado[j] = datoOrdenado[i];
-            datoOrdenado[i]= temp;
-            }
-        }
-        return datoOrdenado;
+  const datoOrdenado = [...elDato];
 
+  for (let i = 0; i < datoOrdenado.length; i++) {
+    for (let j = 0; j < datoOrdenado.length; j++) {
+      if (datoOrdenado[i] > datoOrdenado[j]) {
+      }
+      const temp = datoOrdenado[j];
+      datoOrdenado[j] = datoOrdenado[i];
+      datoOrdenado[i] = temp;
     }
-
-
+  }
+  return datoOrdenado;
+}
 
 const ordenado = ordenaElArray(desordenado);
 console.log(ordenado);
-
-
-
+*/
 /*
 var arr = [ 40, 1, 5, 200 ];
 function comparar ( a, b ){ return a - b; }
 arr.sort( comparar ); 
 */
+/*
+function nuestroCallback(texto) {
+  console.log(texto);
+}
+
+function eco(unCallback) {
+    const contenido ="hola";
+    console.log(contenido)
+  unCallback(contenido);
+}
+eco(nuestroCallback);
+*/
+
+
+
+
+const = desordenado = [7,5,10,3,4]
+function test(params) {
+    desordenado.forEach(callback)
+}
