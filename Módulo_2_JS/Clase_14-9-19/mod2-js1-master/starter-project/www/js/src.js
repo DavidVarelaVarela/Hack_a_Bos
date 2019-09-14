b892477d3848bfd9d9d0f3c460e76536cb08ddb0
DavidVarelaVarela <davidvarela87@gmail.com>
2019-09-14 11:13:00 +0200

    cambios

--- Módulo_2_JS/Clase_14-9-19/mod2-js1-master/starter-project/www/js/src.js
+++ Módulo_2_JS/Clase_14-9-19/mod2-js1-master/starter-project/www/js/src.js
@@ -1,1 +1,31 @@
 'use strict';
+
+
+//Ordenar numero de menos a mayor
+/*
+function ejemplo() {
+let desordenado = [7,5,10,3,4];
+console.log(desordenado.sort(function (a, b){
+    return a - b;
+}))
+console.log(desordenado.sort((a, b) => a - b ))
+*/
+
+
+function ordenarNumeros() {
+    let array1 [7,5,10,3,4]
+    let array2 [7,5,10,3,4]
+    for (let i = 0; i< array1.length; i++) {
+        if (array1[i] > array1[i+1]) {
+array2
+        }
+        
+    }
+    
+}
+
+/*
+var arr = [ 40, 1, 5, 200 ];
+function comparar ( a, b ){ return a - b; }
+arr.sort( comparar ); 
+*/
