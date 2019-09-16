4eb9135e13426dfdcec0f3c0f271de0e4ae74774
DavidVarelaVarela <davidvarela87@gmail.com>
2019-09-16 18:54:20 +0200

    ejercicio piramide

--- Módulo_2_JS/Clase_16-9-19/mod2-js1-master/starter-project/www/js/src.js
+++ Módulo_2_JS/Clase_16-9-19/mod2-js1-master/starter-project/www/js/src.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function buildPiramid(floorNumber){
+for (let i = 0; i < floorNumber; i++) {
+    let floor = '';
+for (let j = 0; j < floorNumber - i -1; j++) {
+    
+        floor + = `` ;
+        for (let j= 0; j <1 +2; j++) {
+
+            floor + = `` 
+        
+
+    }
+    pyramid.push(floor)
+}
+
+};
+  return pyramid  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
