231c1bd649cb3eff8e070b7298ff30dc31dea353
DavidVarelaVarela <davidvarela87@gmail.com>
2019-09-16 19:56:50 +0200

    ejercicio for of

--- Módulo_2_JS/Clase_16-9-19/mod2-js1-master/starter-project/www/js/src.js
+++ Módulo_2_JS/Clase_16-9-19/mod2-js1-master/starter-project/www/js/src.js
@@ -1,35 +1,26 @@
 'use strict';
-
-function buildPiramid(floorNumber){
-for (let i = 0; i < floorNumber; i++) {
-    let floor = '';
-for (let j = 0; j < floorNumber - i -1; j++) {
-    
-        floor + = `` ;
-        for (let j= 0; j <1 +2; j++) {
-
-            floor + = `` 
-        
-
-    }
-    pyramid.push(floor)
+/*
+const data = [1,10,3,14,5];
+const filteredData = data.filter((item) => item =>10);
+console.log(filteredData);
+*/
+/*
+const data = [1,12,3,14,5];
+const reduceData = data.reduce((accumulator,currentvalue)=>( accumulator * currentvalue));
+// eslint-disable-next-line no-console
+console.log(reduceData);
+*/
+/*
+const data = [3,14,7,17,30];
+for (const item of data){
+  // eslint-disable-next-line no-console
+  console.log(item);
 }
-
-};
-  return pyramid  
-}
+*/
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+const names = [Juan, Pablo, Victor, Sara];
+const ages = [21,15, 10,30 ];
+for (const item of users);
+const users = users.map(function (names)  => (ages>18));
+console.log(users);
