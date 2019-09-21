527aa6826345b844118ff17524a2010d9ca71e19
DavidVarelaVarela <davidvarela87@gmail.com>
2019-09-21 11:27:29 +0200

    ejercicio tienda

--- Módulo_2_JS/Clase_21-19-19/mod2-js1-master/starter-project/www/js/src.js
+++ Módulo_2_JS/Clase_21-19-19/mod2-js1-master/starter-project/www/js/src.js
@@ -1,1 +1,47 @@
 'use strict';
+
+class item{
+    constructor ( name, price) {
+        this.name =name;
+        this.price =price;
+    }
+ } 
+
+class user {
+    constructor(name){
+        this.name=name;
+        this.cart=[];
+
+    }
+    addToCart(item){
+this.cart.push(item);
+    }
+}
+
+class shop{
+    static checkout(user){
+
+    for(let  i =0; i < user.cart.length; 1++) {
+        let cantidad = user.cart.filter(item, => {
+            return user.cart [i].name === item.name;
+        })
+            user.cart.[i].amouunt = cantidad.length;
+      }
+    }
+} 
+
+
+const pepe =new user ("pepe");
+const shoes = new item ("shoes, 800");
+pepe.addToCart(shoes);
+pepe.addToCart(shoes);
+shop.checkout(pepe);
+console.log(pepe.cart);
+
+
+
+
+
+
+
+
