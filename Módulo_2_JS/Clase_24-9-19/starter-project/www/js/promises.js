6e2e614b8fc0f3204ba50c45091276c715a93909
DavidVarelaVarela <davidvarela87@gmail.com>
2019-09-28 12:51:47 +0200

    index

--- Módulo_2_JS/Clase_24-9-19/starter-project/www/js/promises.js
+++ Módulo_2_JS/Clase_24-9-19/starter-project/www/js/promises.js
@@ -7,9 +7,22 @@ class Gambler {
       }, prediction);
     });
   }
+static givePrediction(numPredictions){
+    const numPredictions =[];
+
+    for (let i=0; i<numPredictions; i++){
+      const prediction.push{this.givePrediction(());
+    }
+    return prediction;
 }
+
+}
+
 console.log(
   Gambler.givePrediction().then((myNumber) => {
     console.log(myNumber);
   })
 );
+Promise.all(Gambler.givePrediction(10)).then ((values)=>{
+    console.log(values);
+});
