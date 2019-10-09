4492a3582c80f618e6f6a58ec0015daf7973e9e4
DavidVarelaVarela <davidvarela87@gmail.com>
2019-10-09 21:10:17 +0200

    dom basico

---
+++ Modulo_4_JS_2/www/main.js
@@ -0,0 +1,11 @@
+// El objeto Window
+const windowHeight = window.innerHeight;
+const windowWidth = Window.innerwWidth;
+console.log("Altura y anchura del viewport:");
+console.log(windowHeight + "px", windowWidth + "px");
+
+const completeWindow = window.outerHeight;
+const completeWindow = window.outerWidth;
+
+console.log("Altura y anchura de la ventana del navegador:");
+console.log(completewindowHeight + "px", completewindowWidth + "px");
